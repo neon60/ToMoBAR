0f57c3ab56bfd04db523beda5fb226ec3c53487c
Ferenc Tükör <ferenc.tukor@radwaytech.services>
2025-05-07 15:33:59 +0200

    Widen angle range for all results This covers small numerical errors at close to horizontal projections Remove gather_kernel_center_prune that covered the horizontal results

--- tomobar/methodsDIR_CuPy.py
+++ tomobar/methodsDIR_CuPy.py
@@ -120,21 +120,6 @@ class RecToolsDIRCuPy(RecToolsDIR):
         )
 
         gather_kernel_center_prune(
-            grid=(1, int(np.ceil(center_size / 8)), 4 * interpolation_filter_half_size),
-            block=(32, 8, 1),
-            args=(
-                angle_range,
-                theta,
-                np.int32(interpolation_filter_half_size),
-                np.int32(center_size),
-                np.int32(center_size),
-                np.int32(4 * interpolation_filter_half_size),
-                np.int32(detector_width),
-                np.int32(projection_count),
-            ),
-        )
-
-        gather_kernel_center_prune(
             grid=(1, _CENTER_SIZE_MIN / 8, _CENTER_SIZE_MIN),
             block=(32, 8, 1),
             args=(
